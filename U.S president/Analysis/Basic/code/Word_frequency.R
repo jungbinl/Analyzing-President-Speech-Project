861ed458b37d8172976b81eb3e2a3578c1a4b023
jungbinl <jbiny1004@naver.com>
2025-11-07 21:50:02 -0700

    Update and rename U.S president/Analysis/Basic/Word_frequency.R to U.S president/Analysis/Basic/code/Word_frequency.R

--- U.S president/Analysis/Basic/code/Word_frequency.R
+++ U.S president/Analysis/Basic/code/Word_frequency.R
@@ -5,9 +5,14 @@ library(textstem)
 library(scales)
 library(tidytext)
 library(tidyr)
+library(showtext)
 
 total_speech <- read.csv("total_speech.csv")
 after_1960_speech <- read.csv("after_1960_speech.csv")
+
+# change pont
+font_add(family = "a", regular = "Oswald-Regular.ttf")
+showtext_auto()
 
 # get top 10 most use word in the speech(to get stop word)
 nous <- total_speech %>% filter(upos == "NOUN") %>% count(token) %>% as.data.frame()
@@ -15,7 +20,11 @@ nous_data <- nous %>% filter(n > 20, str_count(token) > 1)
 
 top10_total <- nous_data %>% slice_max(n, n = 10, with_ties = T)
 
-ggplot(top10_total, aes(x = reorder(token, n), y = n, fill = token)) + geom_col(show.legend = F) + coord_flip() + geom_text(aes(label = n), hjust = -0.3) +  theme_classic() + labs(title = "top 10 word in the America president inaugural adress", x = NULL, y = NULL) + theme(text = element_text(family = "a"), plot.title = element_text(hjust = 0.5), panel.grid = element_blank(), axis.text.y = element_text(hjust = 1))
+ggplot(top10_total, aes(x = reorder(token, n), y = n, fill = token)) + 
+  geom_col(show.legend = F) + coord_flip() + geom_text(aes(label = n), hjust = -0.3) +  
+  theme_classic() + 
+  labs(title = "top 10 word in the America president inaugural adress", x = NULL, y = NULL) + 
+  theme(text = element_text(family = "a"), plot.title = element_text(hjust = 0.5), panel.grid = element_blank(), axis.text.y = element_text(hjust = 1))
 
 # filtering stop word, too often use 
 nous_data %>% slice_max(n, n = 20, with_ties = T)
@@ -23,7 +32,12 @@ stop_word <- c("government", "people", "country","nation", "time", "citizen", "c
 nous_data_exception <- nous %>% filter(!token %in% stop_word, n > 10, str_count(token) > 1)
 top10_except <- nous_data_exception %>% slice_max(n, n = 10, with_ties = T)
 
-ggplot(top10_except, aes(x = reorder(token, n), y = n, fill = token)) + geom_col(show.legend = F) + coord_flip() + geom_text(aes(label = n), hjust = -0.3) +  theme_classic() + labs(title = "top 10 word in the America president inaugural adress", x = NULL, y = NULL) + theme(text = element_text(family = "a"), plot.title = element_text(hjust = 0.5), panel.grid = element_blank(), axis.text.y = element_text(hjust = 1))
+ggplot(top10_except, aes(x = reorder(token, n), y = n, fill = token)) + geom_col(show.legend = F) + 
+  coord_flip() + 
+  geom_text(aes(label = n), hjust = -0.3) +  
+  theme_classic() + 
+  labs(title = "top 10 word in the America president inaugural adress", x = NULL, y = NULL) + 
+  theme(text = element_text(family = "a"), plot.title = element_text(hjust = 0.5), panel.grid = element_blank(), axis.text.y = element_text(hjust = 1))
 
 # same as this method, count word each party(use same stop word)
 # demo party
@@ -31,6 +45,7 @@ demo_data <- after_1960_speech[after_1960_speech[ ,"party"] == "democratic", ]
 demo_data$token <- tolower(demo_data$token)
 demo_data$token <- lemmatize_words(demo_data$token)
 demo_data_count <- demo_data %>% filter(upos == "NOUN") %>% count(token) %>% as.data.frame()
+demo_data_exception <- demo_data_count %>% filter(!token %in% stop_word, n > 10, str_count(token) > 1)
 
 top10_except_demo <- demo_data_exception %>% slice_max(n, n = 10, with_ties = T) %>% mutate(party = "democratic")
 
@@ -51,3 +66,6 @@ ggplot(top10_total, aes(x = reorder_within(token, n, party), y = n, fill = party
   geom_text(aes(label = n), hjust = -0.1) + 
   labs(title = "top 10 word by party in the US president inaugural adress", x = NULL, y = NULL, caption = "Source: Inaugural Address Text Data") + facet_wrap( ~ party, scales = "free_y", ncol = 2)  + theme_bw() + 
   theme(text = element_text(family = "a"), plot.title = element_text(hjust = 0.5, size = 16), panel.grid = element_blank(), axis.text.y = element_text(hjust = 1), legend.position = "bottom") + scale_x_reordered()
+
+write.csv(top10_total, "word_frequency.csv", row.names = FALSE)
+write.csv(top10_except, "whole_word_frequency.csv", row.names = FALSE)
